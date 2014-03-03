a9602ced27b4712de8349b0be911aab9cfe9561a
Thane Gill <me@thanegill.com>
2014-03-03 11:10:58 -0800

    Added README and installation of squid.conf

---
+++ README.md
@@ -0,0 +1,3 @@
+Installs e2guardian/squid with VISD custion build
+
+    sudo ./install-e2guadian.sh
--- install-e2guardian.sh
+++ install-e2guardian.sh
@@ -10,22 +10,29 @@ fi
 echo "Installing dependencies..."
 apt-get install squid
 
+echo "Installing e2guardian"
+dpkg -i `dirname $0`/e2guardian_2.12.0.12-1_amd64.deb
+
 echo "Copying system tuning..."
-if [[ -d "/etc/sysctl.d/" && -f "tcp_tune.conf" ]]; then
+if [[ -d "/etc/sysctl.d/" && -f "`dirname $0`/tcp_tune.conf" ]]; then
     cp `dirname $0`/tcp_tune.conf /etc/sysctl.d/tcp_tune.conf
 fi
 
-
 # Create log dir and change ownership
-echo "Creating log directory..."
+echo "Creating e2guardian log directory..."
 mkdir -p /var/log/e2guardian
 chown -R nobody:nogroup /var/log/e2guardian/
 
 # Copy init script into place
-echo "Installing init script"
+echo "Installing e2guardian init script"
 cp `dirname $0`/e2guardian-init.d /etc/init.d/e2guardian
 update-rc.d e2guardian defaults
 
 # Copy logrotate script into place
-echo "Installing logrotate script"
+echo "Installing e2guardian logrotate script"
 cp `dirname $0`/e2guardian-logrotate.d /etc/logrotate.d/e2guardian
+
+if [[ -f "`dirname $0`/squid.conf" ]]; then
+    echo "Installing custom squid.conf"
+    cp `dirname $0`/squid.conf /etc/squid/squid.conf
+fi
